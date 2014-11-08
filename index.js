c914f0bab648eaaed62ec6a8f2ec36c2d2a48381
fabiolenine <fabiolenine@icloud.com>
2014-11-08 11:31:43 -0200

    IO
    Retificação

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.socket.emit('new message',msg);
+    socket.emit('new message',msg);
   });
 });
 
