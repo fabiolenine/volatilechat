f3b965d4a2a210783906d4c339be8924efc16639
fabiolenine <fabiolenine@icloud.com>
2014-11-08 11:17:22 -0200

    On
    Retorno da mensagem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app   = require('express')();
+var http  = require('http').Server(app);
+var io    = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.socket.emit('new message',msg);
   });
 });
 
